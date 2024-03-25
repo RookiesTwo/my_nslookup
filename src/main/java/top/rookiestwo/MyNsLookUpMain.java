c4a6bdf81b94d6cd2561bd171e8a907b50d10ffb
肉鸡二号 <rookies_two@yeah.net>
2024-03-25 20:04:55 +0800

    second Commit

--- src/main/java/top/rookiestwo/MyNsLookUpMain.java
+++ src/main/java/top/rookiestwo/MyNsLookUpMain.java
@@ -1,12 +1,14 @@
 package top.rookiestwo;
 
+import org.pcap4j.core.PcapNativeException;
 import org.pcap4j.core.PcapNetworkInterface;
 import org.pcap4j.core.Pcaps;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class MyNsLookUpMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException, PcapNativeException {
         System.out.println("Hello world!");
         InetAddress addr = InetAddress.getByName("192.168.10.100");
         PcapNetworkInterface nif = Pcaps.getDevByAddress(addr);
